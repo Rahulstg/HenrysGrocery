9a1c93869c09da4ee3886a54c72b1b41e11c8a7b
Rahulstg <136368606+Rahulstg@users.noreply.github.com>
2023-06-22 12:25:20 +0530

    Initial project setup

---
+++ src/main/java/com/henrysgrocery/solution/HenrysGroceryApplication.java
@@ -0,0 +1,13 @@
+package com.henrysgrocery.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenrysGroceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HenrysGroceryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrysgrocery/solution/HenrysGroceryApplicationTests.java
@@ -0,0 +1,8 @@
+package com.henrysgrocery.solution;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HenrysGroceryApplicationTests {
+
+}
